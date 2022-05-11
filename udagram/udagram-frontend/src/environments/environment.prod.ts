c43542fa49eddf3b66c218d0621373a3db3be49c
Andrew Milad <94550303+andrewmiladd@users.noreply.github.com>
2022-05-11 21:53:02 +0200

    Update environment.prod.ts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-mgpwttmn.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://server.eba-zjyzshrp.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
